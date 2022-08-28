fcf97a7964b0c32331ff211e51c246e12c98c8e4
Victor Gabriel <victorgabriel08fs@gmail.com>
2022-08-28 12:50:52 -0300

    datetime

--- src/pages/PaymentsPage/CardItem/index.jsx
+++ src/pages/PaymentsPage/CardItem/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CardItem = ({ item }) => {
-    return (<tr className='card-item'><td>{item.id}</td><td>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(item.value))}</td><td>{item.dueDate}</td><td>{item.status === 'REVERSED' ? 'Estornado' : (item.status === 'RECEIVED' ? 'Recebido' : (item.status === 'RECEIVED_OVERDUE' ? 'Recebido com atraso' : (item.status === 'PENDING' ? 'Pendente' : 'Cancelado')))}</td></tr>);
+    return (<tr className='card-item'><td>{item.id}</td><td>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(Number(item.value))}</td><td>{new Date(item.dueDate).toLocaleDateString('pt-BR')+' - '+new Date(item.dueDate).toLocaleTimeString('pt-BR')}</td><td>{item.status === 'REVERSED' ? 'Estornado' : (item.status === 'RECEIVED' ? 'Recebido' : (item.status === 'RECEIVED_OVERDUE' ? 'Recebido com atraso' : (item.status === 'PENDING' ? 'Pendente' : 'Cancelado')))}</td></tr>);
 }
 
 export default CardItem;
